446cc5feaba10edb1577d69c0767f671904681c6
Taylor Monacelli <taylormonacelli@gmail.com>
2024-10-25 16:48:50 -0700

    Github Actions

--- setup.py
+++ setup.py
@@ -1,10 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name="farwas",
-    version="0.1.0",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
-    install_requires=["zoneinfo;python_version<'3.9'"],
-    python_requires=">=3.9",
-)
+setup()
